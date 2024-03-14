d0f6a20b23d5cb20caa1ed6029347f971b356074
Jesslyn <127361835+christaniajess@users.noreply.github.com>
2024-03-14 09:43:56 +0800

    Create Project.py

---
+++ Backend/Project_Management/Project.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_cors import CORS
+from os import environ
+import sys
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+# Get All information about the user 
+
+
+
+
+# get all unique class objects details for a specific user
