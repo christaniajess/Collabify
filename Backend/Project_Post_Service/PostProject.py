1dca4ab8d361cc0c462b439ad5c2d6742da1d150
Jesslyn_Christania <you@example.com>
2024-03-15 17:30:31 +0800

    getting all projects

--- Backend/Project_Post_Service/PostProject.py
+++ Backend/Project_Post_Service/PostProject.py
@@ -1,9 +1,52 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+mysqlconnector://root:root@localhost:3306'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    __tablename__ = 'Project'
+
+    proj_id = db.Column(db.Integer(10), primary_key = True)
+    user_id = db.Column(db.Integer(10))
+    proj_name = db.Column(db.String)
+    proj_image = db.Column(db.String)
+    proj_description = db.Column(db.String)
+
+    def __init__(self, proj_id, user_id, proj_name, proj_image, proj_description):
+        self.proj_id = proj_id
+        self.user_id = user_id
+        self.proj_name = proj_name
+        self.proj_image = proj_image
+        self.proj_description = proj_description
+
+    def json(self):
+        return {
+            "proj_id": self.proj_id, "user_id": self.user_id, "proj_name": self.proj_name,
+            "proj_image": self.proj_image, "proj_description": self.proj_description
+        }
+
+@app.route('/PostProject')
 def get_all():
-    pass
+    project_list = db.session.scalars(db.select(Project)).all()
+    if len(project_list):
+        return jsonify(
+            {
+                "code" : 200,
+                "data":
+                    {
+                        "Projects": [project.json() for project in project_list]
+                    }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404, 
+            "message": "This user has not created a project before."
+    ), 404
 
 
 #get all projects
