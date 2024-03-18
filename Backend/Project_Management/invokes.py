e472aaf700e55c609726ae4152265be9887c88fb
Jesslyn_Christania <you@example.com>
2024-03-18 23:19:14 +0800

    finished complex microservice

--- Backend/Project_Management/invokes.py
+++ Backend/Project_Management/invokes.py
@@ -11,27 +11,27 @@ def invoke_http(url, method='GET', json=None, **kwargs):
        return: the JSON reply content from the http service if the call succeeds;
             otherwise, return a JSON object with a "code" name-value pair.
     """
-    code = 200
-    result = {}
+    # code = 200
+    # result = {}
 
-    try:
-        if method.upper() in SUPPORTED_HTTP_METHODS:
-            r = requests.request(method, url, json = json, **kwargs)
-        else:
-            raise Exception("HTTP method {} unsupported.".format(method))
-    except Exception as e:
-        code = 500
-        result = {"code": code, "message": "invocation of service fails: " + url + ". " + str(e)}
-    if code not in range(200,300):
-        return result
+    # try:
+    #     if method.upper() in SUPPORTED_HTTP_METHODS:
+    #         r = requests.request(method, url, json = json, **kwargs)
+    #     else:
+    #         raise Exception("HTTP method {} unsupported.".format(method))
+    # except Exception as e:
+    #     code = 500
+    #     result = {"code": code, "message": "invocation of service fails: " + url + ". " + str(e)}
+    # if code not in range(200,300):
+    #     return result
 
-    ## Check http call result
-    if r.status_code != requests.codes.ok:
-        code = r.status_code
-    try:
-        result = r.json() if len(r.content)>0 else ""
-    except Exception as e:
-        code = 500
-        result = {"code": code, "message": "Invalid JSON output from service: " + url + ". " + str(e) + str(json)}
+    # ## Check http call result
+    # if r.status_code != requests.codes.ok:
+    #     code = r.status_code
+    # try:
+    #     result = r.json() if len(r.content)>0 else ""
+    # except Exception as e:
+    #     code = 500
+    #     result = {"code": code, "message": "Invalid JSON output from service: " + url + ". " + str(e) + str(json)}
 
-    return result
+    # return result
