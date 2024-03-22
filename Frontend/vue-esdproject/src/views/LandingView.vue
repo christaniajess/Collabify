9a6a77f00a5fb2aebc922640f692172663f134d5
Izz Danial Sharudin <danials.2022@scis.smu.edu.sg>
2024-03-22 19:03:16 +0800

    delete vue

--- Frontend/vue-esdproject/src/views/LandingView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-// import CommonHeaders from '../components/CommonHeaders.vue'
-import Landing from '../components/Landing.vue'
-</script>
-
-<template>
-    <!-- <CommonHeaders title="Landing" /> -->
-    <Landing />
-</template>
