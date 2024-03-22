5c63cd2b9441b8b8147b1222038ac39adc8bd643
Izz Danial Sharudin <danials.2022@scis.smu.edu.sg>
2024-03-22 17:13:33 +0800

    Base Vue

---
+++ Frontend/vue-esdproject/src/views/LandingView.vue
@@ -0,0 +1,9 @@
+<script setup>
+// import CommonHeaders from '../components/CommonHeaders.vue'
+import Landing from '../components/Landing.vue'
+</script>
+
+<template>
+    <!-- <CommonHeaders title="Landing" /> -->
+    <Landing />
+</template>
