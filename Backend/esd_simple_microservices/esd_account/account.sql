d748a7eec8e7f14a8b3a2be07a4b497b08babec5
Izz Danial Sharudin <danials.2022@scis.smu.edu.sg>
2024-03-31 13:47:34 +0800

    updated databases

--- Backend/esd_simple_microservices/esd_account/account.sql
+++ Backend/esd_simple_microservices/esd_account/account.sql
@@ -1,20 +1,94 @@
-DROP DATABASE IF EXISTS `account`;
-CREATE DATABASE IF NOT EXISTS `account` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `account`;
-CREATE TABLE IF NOT EXISTS `account` (
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  `username` TEXT NOT NULL,
-  `acc_type` TEXT NOT NULL,
-  `full_name` TEXT NOT NULL,
-  `email` TEXT NOT NULL,
-  `password` TEXT NOT NULL,  -- Consider using a larger length for passwords
-  `user_photo` TEXT DEFAULT NULL,
-  `interests` TEXT DEFAULT NULL, -- Modified column for user interests
-  `stripe_key` TEXT DEFAULT NULL, -- New column for stripe key
-  PRIMARY KEY (`user_id`)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 30, 2024 at 07:22 AM
+-- Server version: 5.7.39
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `account`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `username` text NOT NULL,
+  `acc_type` text NOT NULL,
+  `full_name` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `user_photo` text,
+  `interests` text,
+  `stripe_key` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Insert sample data
-INSERT INTO `account` (`user_id`, `username`, `acc_type`, `full_name`, `email`, `password`, `user_photo`, `interests`,`stripe_key`) VALUES
-                      (1234567890, 'CannotSeeMe', 'cc', 'John Cena', '1915408967@qq.com', 'iLoveWrestling', NULL, 'wrestling,acting','sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne');
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`user_id`, `username`, `acc_type`, `full_name`, `email`, `password`, `user_photo`, `interests`, `stripe_key`) VALUES
+(100, 'john_doe', 'cc', 'John Doe', '1915408967@qq.com', '100', 'user1.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(101, 'brand_inc', 'brand', 'Brand Inc', '1915408967@qq.com', '101', 'user2.jpg', 'sports,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(102, 'alex_jones', 'cc', 'Alex Jones', '1915408967@qq.com', '102', 'user3.jpg', 'food,travel', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(103, 'brand_max', 'brand', 'Max Corporation', '1915408967@qq.com', '103', 'user4.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(104, 'michael_wilson', 'cc', 'Michael Wilson', '1915408967@qq.com', '104', 'user5.jpg', 'technology,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(105, 'brand_media', 'brand', 'Media Enterprises', '1915408967@qq.com', '105', 'user6.jpg', 'food,books', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(106, 'david_miller', 'cc', 'David Miller', '1915408967@qq.com', '106', 'user7.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(107, 'brandify', 'brand', 'Brandify', '1915408967@qq.com', '107', 'user8.jpg', 'food,travel', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(108, 'chris_anderson', 'cc', 'Chris Anderson', '1915408967@qq.com', '108', 'user9.jpg', 'movies,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(109, 'brand_tech', 'brand', 'Tech Solutions', '1915408967@qq.com', '109', 'user10.jpg', 'food,shopping', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(110, 'paul_rodriguez', 'cc', 'Paul Rodriguez', '1915408967@qq.com', '110', 'user11.jpg', 'food,technology', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(111, 'brand_co', 'brand', 'Co Enterprises', '1915408967@qq.com', '111', 'user12.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(112, 'kevin_garcia', 'cc', 'Kevin Garcia', '1915408967@qq.com', '112', 'user13.jpg', 'music,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(113, 'acme_corp', 'brand', 'Acme Corporation', '1915408967@qq.com', '113', 'user14.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(114, 'tech_data', 'brand', 'Tech Data Corporation', '1915408967@qq.com', '114', 'user15.jpg', 'sports,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(115, 'united_nations', 'brand', 'United Nations', '1915408967@qq.com', '115', 'user16.jpg', 'food,travel', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(116, 'warner_media', 'brand', 'Warner Media', '1915408967@qq.com', '116', 'user17.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(117, 'starbucks', 'brand', 'Starbucks Corporation', '1915408967@qq.com', '117', 'user18.jpg', 'technology,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(118, 'tesla', 'brand', 'Tesla, Inc.', '1915408967@qq.com', '118', 'user19.jpg', 'food,books', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(119, 'amazon', 'brand', 'Amazon.com, Inc.', '1915408967@qq.com', '119', 'user20.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(120, 'nike', 'brand', 'Nike, Inc.', '1915408967@qq.com', '120', 'user21.jpg', 'food,travel', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(121, 'microsoft', 'brand', 'Microsoft Corporation', '1915408967@qq.com', '121', 'user22.jpg', 'movies,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(122, 'apple', 'brand', 'Apple Inc.', '1915408967@qq.com', '122', 'user23.jpg', 'food,shopping', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(123, 'lucy_smith', 'cc', 'Lucy Smith', '1915408967@qq.com', '123', 'user24.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(124, 'james_johnson', 'cc', 'James Johnson', '1915408967@qq.com', '124', 'user25.jpg', 'sports,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(125, 'sophia_williams', 'cc', 'Sophia Williams', '1915408967@qq.com', '125', 'user26.jpg', 'food,travel', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(126, 'liam_brown', 'cc', 'Liam Brown', '1915408967@qq.com', '126', 'user27.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(127, 'olivia_jones', 'cc', 'Olivia Jones', '1915408967@qq.com', '127', 'user28.jpg', 'technology,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(128, 'william_davis', 'cc', 'William Davis', '1915408967@qq.com', '128', 'user29.jpg', 'food,books', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(129, 'emma_miller', 'cc', 'Emma Miller', '1915408967@qq.com', '129', 'user30.jpg', 'food,cars', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(130, 'michael_jackson', 'cc', 'Michael Jackson', '1915408967@qq.com', '130', 'user31.jpg', 'music,food', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(131, 'ava_taylor', 'cc', 'Ava Taylor', '1915408967@qq.com', '131', 'user32.jpg', 'food,shopping', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne'),
+(132, 'alexander_anderson', 'cc', 'Alexander Anderson', '1915408967@qq.com', '132', 'user33.jpg', 'food,technology', 'sk_test_51OuGi52KwIS1b45LvlM5Q8z0AaTmdNohKDWk3pOEKhNbvOmHJmCEU9DWbSab871Yal3Qw5H2QMXDynjOuQC6NLXB00NBUhtZne');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`user_id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- Backend/esd_simple_microservices/esd_collab/collaboration.sql
+++ Backend/esd_simple_microservices/esd_collab/collaboration.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 12, 2020 at 02:17 AM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Host: localhost:8889
+-- Generation Time: Mar 30, 2024 at 07:23 AM
+-- Server version: 5.7.39
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,71 +30,49 @@ USE `collaboration`;
 -- Table structure for table `collaboration`
 --
 
-
-CREATE TABLE IF NOT EXISTS `collaboration` (
-  `cc_id` VARCHAR(100),
-  `cc_name` VARCHAR(100),
-  `brand_id` VARCHAR(100),
-  `collab_title` varchar(20),
-  `collab_status` varchar(20),
-  PRIMARY KEY (`cc_id`, `brand_id`,`collab_title`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
+CREATE TABLE `collaboration` (
+  `cc_id` varchar(100) NOT NULL,
+  `brand_id` varchar(100) NOT NULL,
+  `collab_title` varchar(100) DEFAULT NULL,
+  `collab_status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `collaboration`
 --
 
-INSERT INTO `collaboration` (`cc_id`, `cc_name`, `brand_id`, `brand_name`, `collab_title`, `collab_status`) VALUES
-('4387', 'Alex Johnson', '8912', 'Nike', 'Alex X Nike Spring Collection', 'Pending'),
-('5276', 'Mia Wong', '4956', 'Reebok', 'Mia X Reebok Fitness Line', 'In-Progress'),
-('6345', 'Carlos Gutierrez', '7623', 'Adidas', 'Carlos X Adidas Soccer Edition', 'Done'),
-('7918', 'Samantha Davis', '4067', 'New Balance', 'Samantha X NB Running Tech', 'Completed'),
-('8657', 'Emily Zhang', '6734', 'Puma', 'Emily X Puma Lifestyle Series', 'Pending'),
-('9726', 'Olivia Smith', '2289', 'Saucony', 'Olivia X Saucony Trail Gear', 'In-Progress'),
-('4387', 'Alex Johnson', '7623', 'Adidas', 'Alex X Adidas Winter Capsule', 'Done'),
-('5276', 'Mia Wong', '6734', 'Puma', 'Mia X Puma Heritage Collection', 'Completed'),
-('6345', 'Carlos Gutierrez', '8912', 'Nike', 'Carlos X Nike Performance Wear', 'Pending'),
-('7918', 'Samantha Davis', '2289', 'Saucony', 'Samantha X Saucony Run Series', 'In-Progress'),
-('8657', 'Emily Zhang', '4956', 'Reebok', 'Emily X Reebok Classic Revivals', 'Done'),
-('2254', 'Izzey Miyake', '6504', 'The North Face', 'Miyake X TNF Exploration Gear', 'In-Progress'),
-('9726', 'Olivia Smith', '4067', 'New Balance', 'Olivia X NB Exclusive Drops', 'Completed'),
-('4387', 'Alex Johnson', '2289', 'Saucony', 'Alex X Saucony Limited Edition', 'Pending'),
-('5276', 'Mia Wong', '8912', 'Nike', 'Mia X Nike Air Max Day Special', 'In-Progress'),
-('6345', 'Carlos Gutierrez', '4956', 'Reebok', 'Carlos X Reebok Spartan Series', 'Completed'),
-('7918', 'Samantha Davis', '6734', 'Puma', 'Samantha X Puma Fast Track', 'Pending'),
-('8657', 'Emily Zhang', '8912', 'Nike', 'Emily X Nike Revolution', 'In-Progress'),
-('9726', 'Olivia Smith', '7623', 'Adidas', 'Olivia X Adidas Originals', 'Done'),
-('2254', 'Izzey Miyake', '3201', 'Lululemon', 'Miyake X Lululemon Performance Series', 'Completed'),
-('4387', 'Alex Johnson', '4067', 'New Balance', 'Alex X NB Urban Explorers', 'Completed'),
-('5276', 'Mia Wong', '2289', 'Saucony', 'Mia X Saucony Freedom ISO Launch', 'Pending'),
-('6345', 'Carlos Gutierrez', '4067', 'New Balance', 'Carlos X NB Trailblazers', 'In-Progress'),
-('7918', 'Samantha Davis', '8912', 'Nike', 'Samantha X Nike Heritage Line', 'Done'),
-('8657', 'Emily Zhang', '6734', 'Puma', 'Emily X Puma Classics Reimagined', 'Completed'),
-('9726', 'Olivia Smith', '4956', 'Reebok', 'Olivia X Reebok Innovation Lab', 'Pending'),
-('4387', 'Alex Johnson', '7623', 'Adidas', 'Alex X Adidas Streetwear Edition', 'In-Progress'),
-('5276', 'Mia Wong', '6734', 'Puma', 'Mia X Puma Eco Innovation', 'Done'),
-('6345', 'Carlos Gutierrez', '2289', 'Saucony', 'Carlos X Saucony Cohesion', 'Completed'),
-('7918', 'Samantha Davis', '4067', 'New Balance', 'Samantha X NB Fresh Foam', 'Pending'),
-('9982', 'Noah Lee', '3201', 'Lululemon', 'Noah X Lululemon Athletica Series', 'In-Progress'),
-('1143', 'Isabella Martinez', '4302', 'Patagonia', 'Isabella X Patagonia Outdoor Essentials', 'Done'),
-('3365', 'Ava Smith', '6504', 'The North Face', 'Ava X TNF Summit Series', 'Completed'),
-('4476', 'Matthew Jones', '7605', 'Merrell', 'Matthew X Merrell Trail Run', 'In-Progress'),
-('1143', 'Isabella Martinez', '6504', 'The North Face', 'Isabella X TNF Eco Innovation', 'Pending'),
-('2254', 'Izzey Miyake', '3201', 'Lululemon', 'Miyake X Lululemon Studio Line', 'Completed'),
-('3365', 'Ava Smith', '4302', 'Patagonia', 'Ava X Patagonia Limited Edition', 'In-Progress'),
-('4476', 'Matthew Jones', '7605', 'Merrell', 'Matthew X Merrell Hiking Tech', 'Done'),
-('2254', 'Izzey Miyake', '7605', 'Merrell', 'Miyake X Merrell Wild Hike', 'Done'),
-('9982', 'Noah Lee', '6504', 'The North Face', 'Noah X TNF Advanced Mountain Gear', 'Completed'),
-('1143', 'Isabella Martinez', '7605', 'Merrell', 'Isabella X Merrell Adventure Series', 'Pending'),
-('2254', 'Izzey Miyake', '4302', 'Patagonia', 'Miyake X Patagonia Sustainable Wear', 'In-Progress'),
-('3365', 'Ava Smith', '3201', 'Lululemon', 'Ava X Lululemon Yoga Collection', 'Done');
+INSERT INTO `collaboration` (`cc_id`, `brand_id`, `collab_title`, `collab_status`) VALUES
+('123', '103', 'Tech Data Collaboration', 'Pending'),
+('124', '105', 'Starbucks Marketing Campaign', 'In-progress'),
+('125', '107', 'United Nations Charity Event', 'Review'),
+('126', '109', 'Tesla Product Launch', 'Pending'),
+('127', '111', 'Amazon Prime Video Series Production', 'In-progress'),
+('128', '113', 'Nike Endorsement Deal', 'Review'),
+('129', '115', 'Microsoft Software Development', 'Pending'),
+('130', '117', 'Apple Product Launch Event', 'In-progress'),
+('131', '119', 'Acme Corporation Partnership', 'Review'),
+('132', '121', 'Warner Media Content Creation', 'Pending'),
+('133', '123', 'Lucy Smith Fashion Line Promotion', 'Pending'),
+('134', '125', 'Sophia William Beauty Campaign', 'In-progress'),
+('135', '127', 'Liam Brown Tech Review Video', 'Review'),
+('136', '129', 'Emma Miller Health & Wellness Event', 'Pending'),
+('137', '131', 'Michael Jackson Music Album Launch', 'In-progress'),
+('138', '133', 'Ava Taylor Fitness Apparel Sponsorship', 'Review'),
+('139', '135', 'Alexander Andersons Podcast Collaboration', 'Pending'),
+('140', '137', 'Olivia Jones Lifestyle Blog Feature', 'In-progress'),
+('141', '139', 'William Davis Travel Vlog Partnership', 'Review'),
+('142', '141', 'James Johnson Food Recipe Series', 'Pending');
 
+--
+-- Indexes for dumped tables
+--
 
-
-
-
-
+--
+-- Indexes for table `collaboration`
+--
+ALTER TABLE `collaboration`
+  ADD PRIMARY KEY (`cc_id`,`brand_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
